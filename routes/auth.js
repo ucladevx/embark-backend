3a8d8fea4ce3c2f7e9c87cd9bec22fa7a8e2efc7
Isha R <33170219+irajput@users.noreply.github.com>
2020-11-16 00:20:33 -0800

    removed passport local

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
 const StudentModel = require('../models/student.js');
 
 var jwt=require('jsonwebtoken');

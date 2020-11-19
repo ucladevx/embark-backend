4ba686d83faebedec2592d04acd835ccae0568fb
Isha R <33170219+irajput@users.noreply.github.com>
2020-11-18 21:56:15 -0800

    auth function for router auth.json (need to edit later)

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,16 @@ const { signin, signup } = require("../helpers/auth")
 const authorize=require("../helpers/authMiddleware");
 router.post(
     "/signin",
-    authorize,
+    authorize, (req, res, next) => {
+        StudentModel.findById(req.params.id, (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.status(200).json({
+                    msg: data
+                })
+            }
+        }
 
     
 );

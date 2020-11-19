4973f008c06f07bfd26a079a03c56c6c7c1d2bd4
Isha R <33170219+irajput@users.noreply.github.com>
2020-11-18 20:51:20 -0800

    added authMiddleware.js

--- routes/auth.js
+++ routes/auth.js
@@ -10,34 +10,11 @@ const { signin, signup } = require("../helpers/auth")
 // implement the jwt check and modify the router.post like so:
 // const { existingJwtCheck } = require("../helpers/authMiddleware") 
 // router.post("/signin", existingJwtCheck, signin)
+const authorize=require("../helpers/authMiddleware");
 router.post(
     "/signin",
+    authorize()
     
-    async (req,res,next)=>{
-        passport.authenticate(
-            "signin",
-            async(err,Student)=>{
-                try{
-                    if(eerr || !Student){
-                        const error=new Error('Error');
-                        return next(error);
-                    }
-
-                req.signin(
-                    Student,
-                    async (error) => {
-                        if (error) return next(error);
-                        const body={_id:Student._id,name:Student.name,email:Student.email};
-                        const token=jwt.sign({Student:body},'TOP_SECRET');
-                        return res.json({token});
-                    }
-                );
-            } catch (error) {
-                return next(error);
-            }
-            }
-        ) (req,res,next);
-    }
 );
 
 // probably wanna look into setting up student/signup and club/signup as different endpoints

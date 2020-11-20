0686a3d499b3df6639b64809d3324fc25967a868
Isha R <33170219+irajput@users.noreply.github.com>
2020-11-20 00:24:43 -0800

    added a regex for password

--- helpers/auth.js
+++ helpers/auth.js
@@ -2,6 +2,7 @@ const studentModel = require('../models/student');
 const bcrypt = require('bcryptjs');
 const jwt=require('jsonwebtoken');
 
+
 // signin
 // TODO: check credentials + jwt 
 exports.signin = async function (req, res, next) {
@@ -35,7 +36,10 @@ exports.signin = async function (req, res, next) {
 // TODO: create jwt, hash password, change fields in studentModel
 exports.signup = async function (req, res, next) {
     const { name, email, password,major,year } = req.body;
-    
+    const regExpPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    if(!regExpPassword.test(password)){
+        return res.status(400).send({message:"Password must be 8 characters, must have one Uppercase, one Lowercase, and one special character"});
+    }
         const student = new studentModel({
             name,
             email,
@@ -49,12 +53,10 @@ exports.signup = async function (req, res, next) {
             await student.save()
             res.status(200).send({ auth: true, token: token });
     } catch(e) {
-            if(e.code === 11000) {
-                res.send('That email address is already registered');    
-            }
-            else{
+            
+            
             return res.json({message: e.message});
-            }
+            
         }
     
 }

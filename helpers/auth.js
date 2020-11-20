6e9ffa1c183730c5a5ca9b9c4d23ba699c474fc6
Isha R <33170219+irajput@users.noreply.github.com>
2020-11-19 16:42:59 -0800

    added more fields to signin/signup

--- helpers/auth.js
+++ helpers/auth.js
@@ -4,14 +4,14 @@ var bcrypt = require('bcryptjs');
 // signin
 // TODO: check credentials + jwt 
 exports.signin = async function (req, res, next) {
-    const {email,password}=req.body
+    const {name,email,password,major,year}=req.body
     
     studentModel.findOne({
         email:email,
     }, function(err,studentInfo){
         if(err){
             return res.status(401).json({
-                message: "Signin authentication failed--bad argument"
+                message: "Signin authentication failed--email not found"
             });
         }
         else{
@@ -32,12 +32,14 @@ exports.signin = async function (req, res, next) {
 // signup
 // TODO: create jwt, hash password, change fields in studentModel
 exports.signup = async function (req, res, next) {
-    const { name, email, password } = req.body
+    const { name, email, password,major,year } = req.body
 
     const student = new studentModel({
         name,
         email,
-        password
+        password,
+        major,
+        year
     }) 
 
     const salt=genSaltSync(10);

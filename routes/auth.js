8b5a02a30c1318e033815d120c7d76262df9741d
Sanya <purvi.sanya@gmail.com>
2020-11-29 23:59:29 +0400

    incomplete post endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const StudentModel = require('../models/student.js');
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const { signin, signup } = require("../helpers/auth")
 
 // implement the jwt check and modify the router.post like so:
-const authorize=require("../helpers/authMiddleware");
+// const authorize = require("../helpers/authMiddleware");
 
-
-
-router.post('/signin',authorize,signin);
-
-
-// probably wanna look into setting up student/signup and club/signup as different endpoints
-router.post("/signin", signin)
+router.post('/signin', signin);
 router.post("/signup", signup)
 
 module.exports = router;

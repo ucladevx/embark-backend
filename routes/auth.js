ada73cdc60aede8bef0107ad771a95a12ca3f8d2
Isha R <33170219+irajput@users.noreply.github.com>
2020-12-02 22:11:39 -0800

    nvm im dumb

--- routes/auth.js
+++ routes/auth.js
@@ -6,19 +6,11 @@ const jwt = require('jsonwebtoken');
 const { signin, signup } = require("../helpers/auth");
 const {editProfile,profile}=require("../helpers/student");
 const {editProfileClub,profileClub}=require("../helpers/club");
-
-
-// implement the jwt check and modify the router.post like so:
 const authorize = require("../helpers/authMiddleware");
 
 router.post('/signin', signin);
 router.post("/signup", signup);
 
-router.get('/student/profile',authorize,profile);
-router.post('/student/profile',authorize,editProfile);
-
-router.get('club/profile',authorize,profileClub);
-router.post('/club/profile',authorize,editProfileClub);
 
 
 module.exports = router;

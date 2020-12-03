8d68a9bdfc46ddcf5d2071b2aaef5d9e204afb3e
Isha R <33170219+irajput@users.noreply.github.com>
2020-12-02 23:08:11 -0800

    updated EditProfile function for student

--- helpers/student.js
+++ helpers/student.js
@@ -1,9 +1,24 @@
 const studentModel = require('../models/student')
 
 exports.editProfile = async function (req, res, next) {
-    res.status(501).json({
-        message: "Not implemented yet"
-    })
+    const {name,email,major,year,tags}=req.body;
+    try{
+    const token=req.headers.authorization.split(" ")[1];
+    const decodedToken=jwt.verify(token,req.app.get('secretKey'));
+    const student=studentModel.findOne({email:decodedToken.email});
+    studentModel.updateOne({id:student._id},
+        {$set: {
+            "name": name,
+            "email":email,
+            "major":major,
+            "year":year,
+            "tags":tags
+        }});
+    }
+    catch(err){
+        return res.json({ message: err.message });
+    }
+    res.redirect('/profile');
 }
 
 exports.profile = async function (req, res, next) {

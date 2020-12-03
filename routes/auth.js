56efb5456f494bffd606278d70638c4f970d053a
Isha R <33170219+irajput@users.noreply.github.com>
2020-12-02 21:47:13 -0800

    added endpoints for profile (not built yet)

--- routes/auth.js
+++ routes/auth.js
@@ -6,9 +6,15 @@ const jwt = require('jsonwebtoken');
 const { signin, signup } = require("../helpers/auth")
 
 // implement the jwt check and modify the router.post like so:
-// const authorize = require("../helpers/authMiddleware");
+const authorize = require("../helpers/authMiddleware");
 
 router.post('/signin', signin);
-router.post("/signup", signup)
+router.post("/signup", signup);
+
+router.get('/student/profile');
+router.post('/student/profile');
+router.get('club/profile');
+router.post('/club/profile');
+
 
 module.exports = router;

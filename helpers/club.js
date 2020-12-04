b026a35e037229a5e01ff29c417ad45d7d558ae9
Isha R <33170219+irajput@users.noreply.github.com>
2020-12-03 22:14:45 -0800

    finished club editProfile

--- helpers/club.js
+++ helpers/club.js
@@ -1,9 +1,30 @@
-const studentModel = require('../models/student')
+const clubModel = require('../models/club')
 
+function getClubFromToken(token,email){
+    const decodedToken=jwt.verify(token,req.app.get('secretKey'));
+    return clubModel.findOne({email:decodedToken.email});
+}
 exports.editProfile = async function (req, res, next) {
-    res.status(501).json({
-        message: "Not implemented yet"
-    })
+    const {name,email,tags,description,profilePicURL,coverPicURL,website}=req.body;
+    try{
+    const token=req.headers.authorization.split(" ")[1];
+    const club=getClubFromToken(token);
+    clubModel.updateOne({id:club._id},
+        {$set: {
+            "name": name,
+            "email":email,
+            "tags":tags,
+            "website":website,
+            "description":description,
+            "profilePicURL":profilePicURL,
+            "coverPicURL":coverPicURL
+
+        }});
+    }
+    catch(err){
+        return res.json({message:err.message});
+    }
+    res.redirect('/profile');
 }
 
 exports.profile = async function (req, res, next) {

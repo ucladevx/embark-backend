04489f3c46138594f09474498b08f5f8bc3685e8
Sanya <purvi.sanya@gmail.com>
2021-01-28 16:31:36 +0400

    minor fixes

--- helpers/student.js
+++ helpers/student.js
@@ -1,5 +1,5 @@
 const studentModel = require('../models/student')
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const ObjectID = require('mongodb').ObjectID;
 
 const aws = require("aws-sdk");
@@ -8,60 +8,63 @@ const multerS3 = require("multer-s3");
 const s3 = new aws.S3();
 
 const imageFunction = require("../helpers/image");
-const authorize=require("../helpers/authMiddleware");
+const authorize = require("../helpers/authMiddleware");
 
 const findAndUpdate = async (decodedEmail, updatedFields) => {
-  const student=await studentModel.findOne({email:decodedEmail});
-  const result=await studentModel.updateOne({_id: student._id },updatedFields);
-  returnedStudent=await studentModel.findOne({email:decodedEmail}); //to get the updated student
+  const student = await studentModel.findOne({ email: decodedEmail });
+  console.log(1)
+  console.log(decodedEmail)
+  console.log(student)
+  const result = await studentModel.updateOne({ _id: student._id }, updatedFields);
+  returnedStudent = await studentModel.findOne({ email: decodedEmail }); //to get the updated student
   return returnedStudent;
 }
 
 
 exports.editProfile = async function (req, res, next) {
-   const {name,major,year,tags,bio,linkedIn}=req.body;
-   editableFields={name,major,year,tags,bio,linkedIn};
-   const decodedToken=await authorize(req,res,next);
-   try{
-   const updatedFields = {};
-   Object.keys(editableFields).forEach(key => {
-   if (req.body[key]) {
-       updatedFields[key] = req.body[key];
-   }});
-   const updatedStudent=await findAndUpdate(decodedToken.email, updatedFields);
-   res.send({updatedStudent});
-   }
-   catch(err){
-       return res.json({message:err.message});
-   }
-   
-} 
+  const { name, major, year, tags, bio, linkedIn } = req.body;
+  editableFields = { name, major, year, tags, bio, linkedIn };
+  const decodedToken = await authorize(req, res, next);
+  try {
+    const updatedFields = {};
+    Object.keys(editableFields).forEach(key => {
+      if (req.body[key]) {
+        updatedFields[key] = req.body[key];
+      }
+    });
+    const updatedStudent = await findAndUpdate(decodedToken.email, updatedFields);
+    return res.send({ updatedStudent });
+  }
+  catch (err) {
+    return res.json({ message: err.message });
+  }
 
-exports.profile = async function (req, res, next) {
-    const decodedToken=await authorize(req,res,next);
-    const student=await studentModel.findOne({email:decodedToken.email});
-    res.send({student});
 }
 
-exports.image = async function(req, res, next){
-  const {pictureType}= req.query;
-  const imageURL=await imageFunction(req,res,next);   //gets the aws image URL
-  try{
-    const decodedToken=await authorize(req,res,next);
+exports.profile = async function (req, res, next) {
+  const decodedToken = await authorize(req, res, next);
+  const student = await studentModel.findOne({ email: decodedToken.email });
+  res.send({ student });
+}
+
+exports.image = async function (req, res, next) {
+  const { pictureType } = req.query;
+  const imageURL = await imageFunction(req, res, next);   //gets the aws image URL
+  try {
+    const decodedToken = await authorize(req, res, next);
     let updatedFields;
     console.log(pictureType);
-    if(pictureType==='cover'){
-      updatedFields={coverPicURL:imageURL}
+    if (pictureType === 'cover') {
+      updatedFields = { coverPicURL: imageURL }
     }
-    else{
-      updatedFields={profilePicURL:imageURL}
+    else {
+      updatedFields = { profilePicURL: imageURL }
     }
-    const updatedStudent=await findAndUpdate(decodedToken.email, updatedFields);
-    res.send({updatedStudent});
+    const updatedStudent = await findAndUpdate(decodedToken.email, updatedFields);
+    res.send({ updatedStudent });
   }
-  catch(err)
-  {
-    return res.json({message:err.message});
+  catch (err) {
+    return res.json({ message: err.message });
   }
 }
 

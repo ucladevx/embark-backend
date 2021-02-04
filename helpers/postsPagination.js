3043507b370e4b745994ed484dbd08ace92948f3
Isha R <33170219+irajput@users.noreply.github.com>
2021-02-03 21:56:21 -0800

    something changed

--- helpers/postsPagination.js
+++ helpers/postsPagination.js
@@ -9,15 +9,6 @@ exports.getPostsPage=async(limitNum, nextPage,previousPage,tags,clubs) =>{
     //const {limitNum,nextPage,previousPage}=req.query;
     console.log(limitNum);
     try{
-        
-        // const result = await MongoPaging.find(postModel.collection, 
-        // {
-        //     paginatedField:"timestamp",
-        //     limit:parseInt(limitNum),   //number of pages we want
-        //     sortAscending:true, 
-        //     next:nextPage,       //the next string that is produced after running getPostsPage once
-        //     previous:previousPage
-        // });
         console.log(tags);
         const result=await MongoPaging.find(postModel.collection,
             {
@@ -42,17 +33,7 @@ exports.getPostsPage=async(limitNum, nextPage,previousPage,tags,clubs) =>{
 
              });
         
-        //console.log(result);
-
-        // const result = await MongoPaging.search(postModel.collection, 
-        //     {
-        //         //paginatedField:"timestamp",
-        //         limit:13,   //number of pages we want
-        //         sortAscending:true, 
-        //         next:nextPage,       //the next string that is produced after running getPostsPage once
-        //         previous:previousPage
-        //     });
-       // return result;
+       
        return result;
     } catch(err){
         console.log(err);

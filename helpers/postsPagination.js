f1b7ad4bbd3dcdce22b462c16f039997458ccb4e
Sanya <purvi.sanya@gmail.com>
2021-02-11 09:01:01 +0400

    change pagination order

--- helpers/postsPagination.js
+++ helpers/postsPagination.js
@@ -5,14 +5,14 @@ const MongoPaging = require("mongo-cursor-pagination")
 const ObjectId = require("mongodb").ObjectId;
 
 
-exports.getPostsPage=async(limitNum, nextPage,previousPage,tags,clubs) =>{
+exports.getPostsPage = async (limitNum, nextPage, previousPage, tags, clubs) => {
     //const {limitNum,nextPage,previousPage}=req.query;
     console.log(limitNum);
-    try{
+    try {
         console.log(tags);
-        const result=await MongoPaging.find(postModel.collection,
+        const result = await MongoPaging.find(postModel.collection,
             {
-                query:{
+                query: {
                     $or: [{
                         tags: {
                             $in: tags
@@ -22,20 +22,20 @@ exports.getPostsPage=async(limitNum, nextPage,previousPage,tags,clubs) =>{
                             $in: clubs
                         }
                     }]
-            
-                    
-                },
-                paginatedField:"timestamp",
-                limit:parseInt(limitNum),   //number of pages we want
-                sortAscending:true, 
-                next:nextPage,       //the next string that is produced after running getPostsPage once
-                previous:previousPage
 
-             });
-        
-       
-       return result;
-    } catch(err){
+
+                },
+                paginatedField: "timestamp",
+                limit: parseInt(limitNum),   //number of pages we want
+                sortAscending: false,
+                next: nextPage,       //the next string that is produced after running getPostsPage once
+                previous: previousPage
+
+            });
+
+
+        return result;
+    } catch (err) {
         console.log(err);
         res.send({ message: err.message });
     }

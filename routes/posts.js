bffb71ade81ce4423eb63f6570454b0b47045822
Isha R <33170219+irajput@users.noreply.github.com>
2021-02-17 21:22:36 -0800

    jf q

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
-const { createPosts, getPosts, savePost, getSavedPosts } = require("../helpers/posts")
-// router.post("/create", jwtCheck, create)
-=======
 const { createPosts, getPosts, savePost, getSavedPosts,  
     addPostLike, getPostLikes, addPostComment, getPostComments } = require("../helpers/posts")
->>>>>>> d540e6de8874e3f0093ac013744f3d063056e14c
 
 const { getPostsPage } = require("../helpers/postsPagination")
 const authorize = require("../helpers/authMiddleware");
@@ -27,5 +22,5 @@ router.post("/comments", authorize, addPostComment)
 router.get("/comments", authorize, getPostComments)
 
 router.get("/postsPage", authorize, getPostsPage)
-
+//router.get("/posts",authorize,getPostsPage)
 module.exports = router;

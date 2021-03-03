2e50477e5372973246454cacdec3870d29d1aa10
Isha R <33170219+irajput@users.noreply.github.com>
2021-03-03 13:43:46 -0800

    fixed resources

--- helpers/resources.js
+++ helpers/resources.js
@@ -3,22 +3,17 @@ const multer = require("multer");
 const multerS3 = require("multer-s3");
 
 const s3 = new aws.S3({ httpOptions: { timeout: 10 * 60 * 1000 }});
-const fs = require('fs');
-
-
-//https://stackabuse.com/uploading-files-to-aws-s3-with-node-js/
 
 
 
 module.exports=async(req,res,next) => {
-
+    let files=[];
     return new Promise(async function(resolve,reject){ 
         aws.config.update({
             secretAccessKey: process.env.S3_ACCESS_SECRET,
             accessKeyId: process.env.S3_ACCESS_KEY,
             region: "us-west-2", 
           });
-          let files=[]
           const upload = await multer({
            
             storage: multerS3({
@@ -26,8 +21,9 @@ module.exports=async(req,res,next) => {
               s3,
               bucket: "club-resources-embark",
               metadata: function (req, file, cb) {
-                cb(null, { fieldName: "TESTING_METADATA" });
-                files.push(file);
+                cb(null, { 
+                  fieldName: "TESTING_METADATA"});
+                  files.push(file);
               },
               key: function (req, file, cb) {
                 cb(null, Date.now().toString());
@@ -54,7 +50,6 @@ module.exports=async(req,res,next) => {
             }
             else{
                 let fileLocation=[];
-               // console.log(req.files);
                 if(req.files!=undefined){
                     for (upFile of req.files){
                         fileLocation.push(upFile.location);
@@ -64,7 +59,6 @@ module.exports=async(req,res,next) => {
                 for(i=0;i<fileLocation.length;i++){
                   files[i]["location"]=fileLocation[i];
                 }
-                //console.log("hello");
                 res.send({"files":files});
                 
             }

39d86e49f21fe11b69404b2f2f787be5e47be37d
Satva <satvabshah@gmail.com>
2021-03-03 22:23:28 -0800

    Finished getIndustries and started updating email lookups to ids

--- helpers/student.js
+++ helpers/student.js
@@ -16,7 +16,7 @@ exports.editProfile = async function (req, res, next) {
   editableFields = { name, major, year, tags, bio, linkedIn };
   const token = req.headers.authorization.split(" ")[1];
   const decodedToken = await jwt.verify(token, req.app.get('secretKey'));
-  const student = await studentModel.findOne({ email: decodedToken.email });
+  const student = await studentModel.find({ _id: decodedToken.id });
   try {
     const updatedFields = {};
     Object.keys(editableFields).forEach(key => {
@@ -29,19 +29,19 @@ exports.editProfile = async function (req, res, next) {
   catch (err) {
     return res.json({ message: err.message });
   }
-  returnedStudent = await studentModel.findOne({ email: decodedToken.email });
+  returnedStudent = await studentModel.find({ _id: decodedToken.id });
   res.send({ returnedStudent });
 }
 
 exports.profile = async function (req, res, next) {
   const token = req.headers.authorization.split(" ")[1];
   const decodedToken = await jwt.verify(token, req.app.get('secretKey'));
-  const student = await studentModel.findOne({ email: decodedToken.email });
+  const student = await studentModel.find({ _id: decodedToken.id });
   res.send({ student });
 }
 
 const findAndUpdate = async (decodedEmail, updatedFields) => {
-  const student = await studentModel.findOne({ email: decodedEmail });
+  const student = await studentModel.find({ _id: decodedToken.id });
   console.log(1)
   console.log(decodedEmail)
   console.log(student)
@@ -64,7 +64,7 @@ exports.editProfile = async function (req, res, next) {
         updatedFields[key] = req.body[key];
       }
     });
-    const updatedStudent = await findAndUpdate(decodedToken.email, updatedFields);
+    const updatedStudent = await findAndUpdate(decodedToken.id, updatedFields);
     return res.send({ updatedStudent });
   }
   catch (err) {
@@ -77,17 +77,20 @@ exports.profile = async function (req, res, next) {
   // const decodedToken = await authorize(req, res, next);
   const token = req.headers.authorization.split(" ")[1];
   const decodedToken = jwt.verify(token, req.app.get('secretKey'));
-  const student = await studentModel.findOne({ email: decodedToken.email });
+  const student = await studentModel.find({ _id: decodedToken.id });
   res.send({ student });
 }
 
 exports.image = async function (req, res, next) {
   const { pictureType } = req.query;
   const imageURL = await imageFunction(req, res, next);   //gets the aws image URL
+  const token = req.headers.authorization.split(" ")[1];
+  console.log(token);
+  const decoded = jwt.decode(token);
   try {
     // const decodedToken = await authorize(req, res, next);
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, req.app.get('secretKey'));
+    //let id = decoded.payload.id;
+    console.log(decoded);
     let updatedFields;
     console.log(pictureType);
     if (pictureType === 'cover') {
@@ -96,11 +99,14 @@ exports.image = async function (req, res, next) {
     else {
       updatedFields = { profilePicURL: imageURL }
     }
-    const updatedStudent = await findAndUpdate(decodedToken.email, updatedFields);
-    res.send({ updatedStudent });
+    console.log("it made it here")
+    const updatedStudent = await studentModel.findOneAndUpdate({email: decoded.email}, updatedFields);
+    //const updatedStudent = await studentModel.findOne({email: decoded.email})
+    return res.status(200).json(updatedStudent)
   }
   catch (err) {
-    return res.json({ message: err.message });
+    console.log("something")
+    return res.status(400).json({ message: err.message });
   }
 }
 
@@ -109,13 +115,13 @@ exports.getClubs = async function (req, res) {
   // pull email from jwt
   const token = req.headers.authorization.split(" ")[1];
   const decoded = jwt.decode(token, { complete: true });
-  let email = decoded.payload.email;
-  console.log('Request made from:', email)
+  let id = decoded.payload.id;
+  console.log('Request made from:', id)
 
   // Find which clubs the student follows
   let clubs = []
   try {
-    clubs = await studentModel.findOne({ email: email }, 'clubs');
+    clubs = await studentModel.findOne({ _id: id }, 'clubs');
     console.log('Clubs:', clubs)
   }
   catch (err) {
@@ -169,3 +175,29 @@ exports.getClubs = async function (req, res) {
     clubDetails
   })
 */
+
+exports.getIndustries = async function (req, res) {
+  
+  // pull email from jwt
+  const token = req.headers.authorization.split(" ")[1];
+  const decoded = jwt.decode(token, { complete: true });
+  let id = decoded.payload.id;
+  console.log('Request made from:', id)
+
+  // Find which industries the student follows
+  let industries = []
+  try {
+    industries = await studentModel.findOne({ _id: id }, 'tags'); // TODO: make this industries
+    console.log('Industries:', industries)                             // "tags" is industries
+    return res.status(200).json({
+      message: "Query successful",
+      industries
+    })
+  }
+  catch (err) {
+    return res.status(400).json({
+      message: err.message 
+    })
+  }
+}
+
--- routes/student.js
+++ routes/student.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-const { editProfile, profile, image, getClubs } = require("../helpers/student")
+const { editProfile, profile, image, getClubs, getIndustries } = require("../helpers/student")
 const authorize = require("../helpers/authMiddleware");
 
 router.post("/profile", authorize, editProfile)
@@ -12,5 +12,7 @@ router.post("/profile/image", authorize, image)
 // router.get("/profile/image")
 
 router.get("/getClubs", authorize, getClubs)
+
+router.get("/getIndustries", authorize, getIndustries)
 
 module.exports = router;

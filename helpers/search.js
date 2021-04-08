aa75644208645757b5a4987b8e314c5e016452eb
Evan Wang <evan.wang.2000@gmail.com>
2021-04-07 22:02:45 -0700

    removed extra program comments

--- helpers/search.js
+++ helpers/search.js
@@ -3,11 +3,11 @@ const studentModel = require('../models/student')
 
 
 exports.completeSearch = async function (req, res) {
-    console.log("asdjfkl;")
     try{
       const {searchString} = req.body;
-      let searchResultClubs = await clubModel.find({$text: {$search: searchString}})
       let resultsList = []
+      // search through club DB
+      let searchResultClubs = await clubModel.find({$text: {$search: searchString}})
       for (i = 0; i < searchResultClubs.length; i++) {
         resultsList.push({
             name: searchResultClubs[i].name,
@@ -15,6 +15,7 @@ exports.completeSearch = async function (req, res) {
             accountType: 'club'
         })
       }
+      // search through students DB
       let searchResultStudents = await studentModel.find({$text: {$search: searchString}})
       for (i = 0; i < searchResultStudents.length; i++) {
         resultsList.push({
@@ -22,7 +23,7 @@ exports.completeSearch = async function (req, res) {
             id: searchResultStudents[i]._id,
             accountType: 'student'})
       }
-      // returns an array of objects that match
+      // returns a list of objects that match and the size of the list
       res.status(200).json({
         queries: resultsList,
         numQueries: resultsList.length

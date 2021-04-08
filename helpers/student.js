dbcd98859659fbb5e21f4fe127ffd636291b8e99
Evan Wang <evan.wang.2000@gmail.com>
2021-04-07 22:10:14 -0700

    im just bad at github

--- helpers/student.js
+++ helpers/student.js
@@ -182,30 +182,3 @@ exports.getFollowedClubs = async function (req, res) {
     });
   }
 }
-<<<<<<< HEAD
-=======
-
-
-exports.search = async function (req, res) {
-  try{
-    const {searchString} = req.body;
-    let searchResult = await studentModel.find({$text: {$search: searchString}})
-    // returns an array of objects that match
-    console.log(searchResult.length)
-    // console.log(searchResult)
-    var resultsList = [];
-    for (i = 0; i < searchResult.length; i++) {
-      resultsList.push({name: searchResult[i].name, id: searchResult[i]._id})
-    }
-    console.log(resultsList)
-    res.status(200).json({
-      result: resultsList
-    })
-  }catch(e) {
-    res.status(400).json({
-      message: e.message
-    })
-  }
-}
-
->>>>>>> f24367fba352c2650c31af5fb29c538ecf24aa36

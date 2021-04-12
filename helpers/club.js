da31aed43426b6b4a39f5e8254dd978788034bf4
Satva <satvabshah@gmail.com>
2021-04-12 16:16:11 -0700

    Refactored club endpoints from email to id

--- helpers/club.js
+++ helpers/club.js
@@ -35,7 +35,7 @@ exports.editProfile = async function (req, res, next) {
       }
     });
     //changing tags
-    const club = await clubModel.findOne({ email: decodedToken.email });
+    const club = await clubModel.findOne({ _id: decodedToken.id });
     var changeTags = club.tags;
     if (tags) {
       changeTags = changeField(tags, club.tags, changeTags);
@@ -53,7 +53,7 @@ exports.profile = async function (req, res, next) {
   const token = req.headers.authorization.split(" ")[1];
   const decodedToken = jwt.verify(token, req.app.get("secretKey"));
   const club = await clubModel.findOne(
-    { email: decodedToken.email },
+    { _id: decodedToken.id },
     { password: 0 }
   );
   res.send({ club });
@@ -143,7 +143,7 @@ exports.discover = async function (req, res) {
   let email = decodeToken(req);
   let user;
   try {
-    user = await studentModel.findOne({ email: email });
+    user = await studentModel.findOne({ _id: id });
   } catch (err) {
     return res.status(400).json({
       message: err.message,
@@ -185,7 +185,7 @@ exports.discover = async function (req, res) {
 exports.getResources = async function (req, res) {
   const token = req.headers.authorization.split(" ")[1];
   const decodedToken = jwt.verify(token, req.app.get("secretKey"));
-  const getClub = await clubModel.findOne({ email: decodedToken.email });
+  const getClub = await clubModel.findOne({ _id: decodedToken.id });
   console.log(getClub);
   res.status(200).json({ success: true, resources: getClub["resources"] });
 };

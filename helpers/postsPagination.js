25911cf4290fdf064165cec9767340c8760c3712
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2021-11-15 21:23:34 -0800

    added curly braces for getPaginatedPost function and changed the parameter for getPaginatedPost function

--- helpers/postsPagination.js
+++ helpers/postsPagination.js
@@ -1,50 +1,61 @@
 const postModel = require("../models/post");
 const MongoPaging = require("mongo-cursor-pagination");
 
-exports.getPostsPage = async ( {
+exports.getPostsPage = async ({
   limit,
-  nextPage1,
-  previousPage1,
-  nextPage2,
-  previousPage2,
-  nextPage3,
-  previousPage3,
+  nextPage,
+  previousPage,
   tags,
   clubs,
-  interactedPosts,
-  likedPosts,
-  commentedPosts}
+  // nextPage1,
+  // previousPage1,
+  // nextPage2,
+  // previousPage2,
+  // nextPage3,
+  // previousPage3,
+  // tags,
+  // clubs,
+  // interactedPosts,
+  // likedPosts,
+  // commentedPosts
+}
 ) => {
   try {
-    let level_1 = {
-      nextPage: nextPage1,
-      previousPage: previousPage1,
-      interactedPosts: interactedPosts || [],
-      likedPosts: likedPosts || [],
-      commentedPosts: commentedPosts || []
-    };
-
-    let level_2 = {
-      nextPage: nextPage2,
-      previousPage: previousPage2,
-      interactedPosts: [],
-      likedPosts: likedPosts || [],
-      commentedPosts: commentedPosts || []
-    };
-
-    let level_3 = {
-      nextPage: nextPage3,
-      previousPage: previousPage3,
+    let level = {
+      nextPage: nextPage,
+      previousPage: previousPage,
       interactedPosts: [],
       likedPosts: [],
-      commentedPosts: []
-    };
+      commentedPosts: [],
+    }
+    // let level_1 = {
+    //   nextPage: nextPage1,
+    //   previousPage: previousPage1,
+    //   interactedPosts: interactedPosts || [],
+    //   likedPosts: likedPosts || [],
+    //   commentedPosts: commentedPosts || []
+    // };
 
-    const level_arr = [level_1, level_2, level_3];
+    // let level_2 = {
+    //   nextPage: nextPage2,
+    //   previousPage: previousPage2,
+    //   interactedPosts: [],
+    //   likedPosts: likedPosts || [],
+    //   commentedPosts: commentedPosts || []
+    // };
 
+    // let level_3 = {
+    //   nextPage: nextPage3,
+    //   previousPage: previousPage3,
+    //   interactedPosts: [],
+    //   likedPosts: [],
+    //   commentedPosts: []
+    // };
+
+    // const level_arr = [level_1, level_2, level_3];
+    const level_arr = [level];
     const tags1 = tags || [];
     const clubs1= clubs || [];
-
 
     for (let i = 1; i <= 3; i++) {
       let posts = await postsQuery(level_arr[i - 1], limit, tags1, clubs1);
@@ -70,6 +81,7 @@ async function postsQuery(level_obj, limitNum, tags, clubs) {
     nextPage,
     previousPage,
   } = level_obj;
+
 
   const posts = await MongoPaging.find(postModel.collection, {
     query: {
